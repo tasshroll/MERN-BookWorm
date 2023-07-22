7909418321690a58a53dec04066194c802044ed7
Tifni Shroll <tasshroll@gmail.com>
2023-07-21 18:19:46 -0700

    modify for  searchBooks to use mutation to SAVE_BOOK

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -88,7 +88,7 @@ const SearchBooks = () => {
     }
 
     try {
-      // const response = await saveBook(bookToSave, token);
+      // const response = await saveBook(bookToSave, token); // removed
       
       // Execute the saveBook mutation
       const { data } = await saveBook({ variables: { input: bookToSave } }); 
@@ -96,10 +96,6 @@ const SearchBooks = () => {
       if (!data || !data.saveBook) {
         throw new Error('something went wrong!');
       }
-
-      // if (!response.ok) {
-      //   throw new Error('something went wrong!');
-      // }
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
