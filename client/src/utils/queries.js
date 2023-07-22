f688fbce19c91500336e92b5a4c0c2a311eb2848
Tifni Shroll <tasshroll@gmail.com>
2023-07-21 18:09:08 -0700

    add queries.js file with GET_ME GraphQL query

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,12 +1,12 @@
 import { gql } from '@apollo/client';
 
+// GraphQL query for "me" to return a user
 export const GET_ME = gql`
   query me {
     users {
       _id
       name
       email
-      password
       savedBooks
     }
   }
